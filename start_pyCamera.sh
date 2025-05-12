1e8bdcf4c9fff487073f6de48ad15b319b0b99c9
dani <dvillasc@gmail.com>
2025-05-11 21:01:37 -0400

    actualizado parametros de la inicializacion de la camara

--- start_pyCamera.sh
+++ start_pyCamera.sh
@@ -9,7 +9,7 @@ source "$PROJECT_DIR/.venv/bin/activate"
 antimicrox --hidden --profile "$PROJECT_DIR/botones.joystick.amgp" & ANTIMICROX_PID=$!
 
 # Ejecutar el programa
-python3 "$PROJECT_DIR/main.py" --o O
+python3 "$PROJECT_DIR/main.py" -o O -q 1
 
 # Finalizar antimicrox
 kill $ANTIMICROX_PID
