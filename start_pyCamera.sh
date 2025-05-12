7e5d24e3454d60e059ebe9a36ee09222d559de78
dani <dvillasc@gmail.com>
2025-05-11 20:28:01 -0400

    script de inicio y archivo de configuracion de botones

---
+++ start_pyCamera.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PROJECT_DIR="/home/raspi5/pyCamera"
+
+# Activar entorno virtual
+source "$PROJECT_DIR/.venv/bin/activate"
+
+# Lanzar antimicrox
+antimicrox --hidden --profile "$PROJECT_DIR/botones.joystick.amgp" & ANTIMICROX_PID=$!
+
+# Ejecutar el programa
+python3 "$PROJECT_DIR/main.py" --o O
+
+# Finalizar antimicrox
+kill $ANTIMICROX_PID
